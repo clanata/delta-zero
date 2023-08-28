112ba2b97f31d378c9198d62c52fc0a8d5c01418
Cameron Lanata <cameron.lanata@delta.com>
2023-08-28 14:57:15 -0400

    Initial Commit

---
+++ Delta Zero/Delta_ZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Delta_ZeroApp.swift
+//  Delta Zero
+//
+//  Created by Lanata, Cameron T on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Delta_ZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
